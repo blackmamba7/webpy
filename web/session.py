9da31a20514d53f3540ed101e5f98eea7d767337
blackmamba7 <41579421+blackmamba7@users.noreply.github.com>
2018-08-02 10:46:16 +0530

    Update session.py

--- web/session.py
+++ web/session.py
@@ -162,6 +162,7 @@ class Session(object):
 
     def _valid_session_id(self, session_id):
         rx = utils.re_compile('^[0-9]+$')
+        #error
         return rx.match(session_id)
         
     def _cleanup(self):
